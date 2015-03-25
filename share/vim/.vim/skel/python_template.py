ab4336610b988dd475078e0ab3521731361ed70d
Chih-Min Chao <cmchao@gmail.com>
2015-03-25 23:44:38 +0800

    share/vim : refine pythone skeleton

--- share/vim/.vim/skel/python_template.py
+++ share/vim/.vim/skel/python_template.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import traceback
 
 def run():
     pass
 
 
 if __name__ == "__main__":
-    run()
-
+    try:
+        run()
+    except SystemExit as exp:
+        if exp.code == 0:
+            raise
+        else:
+            print traceback.print_exc(exp)
+    except Exception as exp:
+        print traceback.print_exc(exp)
