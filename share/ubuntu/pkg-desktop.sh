fbbfc186d6db97e353ae7317aebee71d58cf6a19
Chih-Min Chao <cmchao@marvell.com>
2015-10-19 12:34:40 +0800

    share/ubuntu : add more required packages

--- share/ubuntu/pkg-desktop.sh
+++ share/ubuntu/pkg-desktop.sh
@@ -4,13 +4,14 @@
 #desktop
 pkg_dev="build-essential vim tig git cmake tmux clang gcin byobu pyflakes"
 pkg_dev="$pkg_dev fonts-noto wine byobu keepass2 tree doxygen"
-pkg_sa="aptitude iotop gparted synaptic curl rar"
+pkg_sa="aptitude iotop gparted synaptic curl rar iftop htop"
 pkg_media="qbittorrent smplayer shutter"
 pkg_lib_common="autotools-dev automake libtool"
 pkg_lib_pcman="libglib2.0-dev libgtk2.0-dev libltdl-dev intltool" 
 pkg_lib_qemu="libfdt-dev libgles2-mesa-dev libsasl2-dev libjpeg-turbo8-dev libspice-server-dev"
 pkg_gem5="scons mercurial hg-fast-export swig libprotobuf-dev protobuf-compiler python-dev libgoogle-perftools-dev"
+pkg_pkg="pip"
 
-pkg_all="$pkg_dev $pkg_sa $pkg_media $pkg_lib_common $pkg_lib_pcman $pkg_lib_qemu $pkg_gem5"
+pkg_all="$pkg_dev $pkg_sa $pkg_media $pkg_lib_common $pkg_lib_pcman $pkg_lib_qemu $pkg_gem5 $pkg_pkg"
 
 sudo apt-get install $pkg_all
