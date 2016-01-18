a8d3ea25b853b50fe760b7ad56fceb6543606c8c
Chih-Min Chao <cmchao@marvell.com>
2016-01-18 11:40:48 +0800

    share/ubuntu : add root permission but dont' hardcode sudo in script

--- share/ubuntu/pkg-desktop.sh
+++ share/ubuntu/pkg-desktop.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 #set -x
 
+# detect root permission
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
 #add ppa
 ppa_all="ppa:git-core/ppa"
 for item in $ppa_all; do
-    sudo add-apt-repository $item
+    add-apt-repository $item
 done
 
 #add i386 support
 #reference : http://askubuntu.com/questions/454253/how-to-run-32-bit-app-in-ubuntu-64-bit
-sudo dpkg --add-architecture i386
-sudo apt-get update
-sudo apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
+dpkg --add-architecture i386
+apt-get update
+apt-get install libc6:i386 libncurses5:i386 libstdc++6:i386
 
 
 #desktop
@@ -29,4 +35,4 @@ pkg_pkg="pip"
 
 pkg_all="$pkg_dev $pkg_sa $pkg_compress $pkg_media $pkg_lib_common $pkg_lib_pcman $pkg_lib_qemu $pkg_gem5 $pkg_pkg"
 
-sudo apt-get install $pkg_all
+apt-get install $pkg_all
